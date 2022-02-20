b24beb72fefbb037669d710b6e25fe9605125beb
Yash <yash1202upadhyay@gmail.com>
2022-02-20 14:56:52 +0530

    Created Base Template

--- frtsite/views.py
+++ frtsite/views.py
@@ -3,4 +3,7 @@ from flask import Blueprint, render_template,request,flash
 views = Blueprint('views',__name__)
 @views.route('/')
 def home():
+    v = 0
+    for c in range(1,100):
+        v=c
     return render_template("home.html")
